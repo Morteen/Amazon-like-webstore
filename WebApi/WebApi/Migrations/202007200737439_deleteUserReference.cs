92facf7e96d5985a5bea948f02649467cab0c91e
Morten <mortenolsen4@gmail.com>
2020-07-22 10:03:51 +0200

    Lagrer og ser hva som skjer

---
+++ WebApi/WebApi/Migrations/202007200737439_deleteUserReference.cs
@@ -0,0 +1,20 @@
+namespace WebApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class deleteUserReference : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Orders", "UserId", "dbo.Users");
+            DropIndex("dbo.Orders", new[] { "UserId" });
+        }
+        
+        public override void Down()
+        {
+            CreateIndex("dbo.Orders", "UserId");
+            AddForeignKey("dbo.Orders", "UserId", "dbo.Users", "UserId", cascadeDelete: true);
+        }
+    }
+}
